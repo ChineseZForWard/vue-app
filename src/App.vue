2bd9dec842417619f52dfc9194e000e7fd09c0eb
zhangfuwei <865820565@qq.com>
2021-07-21 00:54:34 +0800

    初版本

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,51 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+    <div id="app">
+        {{msg}}
+        <div>
+            <input type="text" v-model="info">
+            <button v-on:click="handleClick">添加</button>
+        </div>
+        <ul>
+            <todo-item v-for="item in list" v-bind:key="item">
+                <template v-slot:slot_item="itemProps">
+                    <span v-bind:style="{fontSize: '20px', color: itemProps.checked ? 'red': 'blue'}">{{item}}</span>
+                </template>
+            </todo-item>
+        </ul>
+    </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+    import TodoItem from "./components/TodoItem";
 
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+    export default {
+        name: 'App',
+        components: {
+            TodoItem
+        },
+        data() {
+            return {
+                msg: "Hello ZhangFuWei",
+                info: "",
+                list: []
+            }
+        },
+        methods: {
+            handleClick() {
+                this.list.push(this.info)
+                this.info = ""
+            }
+        }
+    }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+    #app {
+        font-family: Avenir, Helvetica, Arial, sans-serif;
+        -webkit-font-smoothing: antialiased;
+        -moz-osx-font-smoothing: grayscale;
+        text-align: center;
+        color: #2c3e50;
+        margin-top: 60px;
+    }
 </style>
---
+++ src/components/TodoItem.vue
@@ -0,0 +1,24 @@
+<template>
+    <li class="theItem">
+        <input type="checkbox" v-model="checked">
+        <slot name="slot-item" v-bind="{checked}"></slot>
+    </li>
+</template>
+
+<script>
+    export default {
+        name: "TodoItem",
+        props: ['the_item'],
+        data() {
+            return {
+                checked: false
+            }
+        }
+    }
+</script>
+
+<style scoped>
+    .theItem {
+        color: red;
+    }
+</style>
